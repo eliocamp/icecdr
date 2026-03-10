0433323bbfa3b047b2f6175c210c09abec3ea34b
Elio Campitelli <eliocampitelli@gmail.com>
2026-03-10 11:35:47 +1100

    Adds cached data to test fixes

--- tests/testthat/test-fix-grid.R
+++ tests/testthat/test-fix-grid.R
@@ -1,6 +1,6 @@
 cdo_installed <- system("cdo -V", ignore.stdout = TRUE) == 0
 skip_if(!cdo_installed)
-options(CDR_DONT_DOWNLOAD = FALSE)
+
 test_that("fix grid works", {
   file_og <- cdr_antarctic_monthly(
     c("2022-01", "2022-01"),
@@ -17,4 +17,47 @@ test_that("fix grid works", {
   file <- suppressWarnings(cdr_fix_grid(file))
 
   expect_true(file_is_valid_grid(file))
+})
+
+
+test_that("fix names works", {
+  file_og <- cdr_antarctic_monthly(
+    c("2022-01", "2022-01"),
+    dir = "test_data",
+    variables = c("aice", "aice_bt"),
+    version = 4,
+    use_cache = TRUE
+  )
+
+  file <- tempfile()
+  file.copy(file_og, file)
+
+  file <- cdr_fix_names(file)
+
+  get_vars <- function(file) {
+    trimws(rcdo::cdo_execute(rcdo::cdo_showname(file)))
+  }
+
+  expect_equal(
+    get_vars(file),
+    "aice aice_bt"
+  )
+
+  files <- replicate(2, {
+    file <- tempfile()
+    file.copy(file_og, file)
+    file
+  })
+
+  files <- cdr_fix_names(files)
+
+  expect_equal(
+    get_vars(files[1]),
+    "aice aice_bt"
+  )
+
+  expect_equal(
+    get_vars(files[2]),
+    "aice aice_bt"
+  )
 })
--- tests/testthat/test-fixes.R
+++ tests/testthat/test-fixes.R
@@ -1,14 +1,33 @@
 cdo_installed <- system("cdo -V", ignore.stdout = TRUE) == 0
 skip_if(!cdo_installed)
-options(CDR_DONT_DOWNLOAD = FALSE)
+
+test_that("fix grid works", {
+  file_og <- with_no_download(cdr_antarctic_monthly(
+    c("2022-01", "2022-01"),
+    dir = "test_data",
+    use_cache = TRUE
+  ))
+  file <- tempfile()
+  file.copy(file_og, file)
+
+  file_is_valid_grid <- function(file) {
+    grid_is_valid(grid_parse(rcdo::cdo_execute(rcdo::cdo_griddes(file))))
+  }
+
+  file <- suppressWarnings(cdr_fix_grid(file))
+
+  expect_true(file_is_valid_grid(file))
+})
+
+
 test_that("fix names works", {
-  file_og <- cdr_antarctic_monthly(
+  file_og <- with_no_download(cdr_antarctic_monthly(
     c("2022-01", "2022-01"),
     dir = "test_data",
     variables = c("aice", "aice_bt"),
     version = 4,
     use_cache = TRUE
-  )
+  ))
 
   file <- tempfile()
   file.copy(file_og, file)
